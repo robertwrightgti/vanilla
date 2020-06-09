ded2987e7f64bc6d1893ab835fd0312de1d3e19f
Rob Wright <rob.wright@groupgti.com>
2020-06-09 16:05:11 +0100

    adding new components into public-api

--- projects/components/src/public-api.ts
+++ projects/components/src/public-api.ts
@@ -4,4 +4,6 @@
 
 export * from './lib/components.service';
 export * from './lib/components.component';
+export * from './lib/comp1/comp1.component';
+export * from './lib/comp2/comp2.component';
 export * from './lib/components.module';
--- projects/elements/src/app/app.module.ts
+++ projects/elements/src/app/app.module.ts
@@ -6,12 +6,9 @@ import { createCustomElement } from '@angular/elements';
 // other project modules
 import {
   ComponentsModule,
-
-  // Comp1Component,
-  // Comp2Component
+  Comp1Component,
+  Comp2Component,
 } from 'components';
-import { Comp1Component } from 'projects/components/src/lib/comp1/comp1.component';
-import { Comp2Component } from 'projects/components/src/lib/comp2/comp2.component';
 
 
 @NgModule({
