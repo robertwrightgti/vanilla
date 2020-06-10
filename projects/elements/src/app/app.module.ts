cab94e58ecbc26b847bd5b5fe3f9d772448e6bb6
Rob Wright <rob.wright@groupgti.com>
2020-06-10 16:47:50 +0100

    organising components

--- projects/elements/src/app/app.module.ts
+++ projects/elements/src/app/app.module.ts
@@ -29,13 +29,20 @@ export class AppModule {
   }
 
   ngDoBootstrap() {
+    /**
+     * this is where we publish things as angular elements
+     * - make sure you name them differently to their internal names
+     * - only publish the elements you actually need
+     *    (in this case, the activity feed ui)
+     */
+
     const el1 = createCustomElement(Comp1Component, { injector: this.injector });
-    customElements.define('lib-comp1', el1);
+    customElements.define('element-comp1', el1);
 
     const el2 = createCustomElement(Comp2Component, { injector: this.injector });
-    customElements.define('lib-comp2', el2);
+    customElements.define('element-comp2', el2);
 
     const el3 = createCustomElement(Comp3Component, { injector: this.injector });
-    customElements.define('lib-comp3', el3);
+    customElements.define('element-comp3', el3);
   }
 }
